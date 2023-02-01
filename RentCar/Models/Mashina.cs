c14104d6130adf84ce1dbcab77f091cc0798c90a
KhMansur <mxamrayev888@gmail.com>
2023-02-01 19:05:29 +0500

    Добавьте файлы проекта.

---
+++ RentCar/Models/Mashina.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentCar.Models
+{
+    public class Mashina
+    {
+        public int Id { get; set; }
+
+        public string Model { get; set; }
+
+        public string Nomer { get; set; }
+
+        public int Narxi { get; set; }
+
+        public int  Yili { get; set; }
+
+        public string Rangi { get; set; }
+
+        public int MijozId { get; set; }
+
+        public bool Bandmi { get; set; } = false;
+    }
+}
---
+++ RentCar/Models/Mijoz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentCar.Models
+{
+    public class Mijoz
+    {
+        public int Id { get; set; }
+
+        public string  Ismi { get; set; }
+
+        public string Passport { get; set; }
+
+        public string GuvohnomaNomeri { get; set; }
+    }
+}
